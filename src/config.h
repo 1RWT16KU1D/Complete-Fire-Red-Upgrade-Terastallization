54d9b02b8a08b788627f8c9e013be21f9f47d4e9
Shiny Miner <falanshhitkedia123@gmail.com>
2025-04-29 17:29:16 +0530

    Add FLAG_TERA

--- src/config.h
+++ src/config.h
@@ -335,6 +335,6 @@ enum //These vars need to be one after the other (hence the enum)
 //#define PAYDAY_MONEY_CAP //Pay Day and G-Max Gold Rush should cap at $99999 per battle
 //#define QUICK_CLAW_PROCING_ALWAYS_ALLOWS_FLEEING //Fleeing from wild battles will never fail the turn a Quick Claw activates
 //#define FROSTBITE //Replaces the Freeze status condition with the Frostbite status condition from PLA
-
+#define FLAG_TERA 0xA07
 /* DexNav Options */
 //See "include/new/dexnav_config.h"
